0ac283be249818199e7625bd8f26515ed66966aa
xps-admin <strategicmindsadvisory@gmail.com>
2026-07-09 17:27:53 -0400

    fix: restore digital bid as working route

--- app/digital-bid/page.tsx
+++ app/digital-bid/page.tsx
@@ -1,5 +1,2 @@
-import ApprovedMockupRoute from '@/components/ApprovedMockupRoute';
-
-export default function Page() {
-  return <ApprovedMockupRoute page="digitalBid" />;
-}
+import ReferencePage from '@/components/ReferencePage';
+export default function Page(){return <ReferencePage pageKey="bid" mode="bid"/>}
