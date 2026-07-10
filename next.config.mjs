a3d948921b160f4e5ba64f05b57a8240c96afd81
xps-admin <strategicmindsadvisory@gmail.com>
2026-07-10 03:16:43 -0400

    fix: ignore eslint during build since it is not a dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: false },
   eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: false },
 };
 export default nextConfig;
